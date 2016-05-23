ada54812d0386e3658026d9df40ed01007a4f7c3
Hiram Huang <chown9835@gmail.com>
2016-05-23 14:02:52 +0000

    Initial WordPress pluign mail file.

---
+++ wp-content-restriction.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * WP Content Restriction
+ *
+ * Plugin Name: WP Content Restriction
+ * Description: Restrict your content, pass after read notice or alert.
+ * Author: Hiram Huang <me@hiram.tw>
+ * Author URI: https://www.facebook.com/naxqihao
+ * Version: 0.1
+ * Text Domain: wp-content-restriction
+ * Domain Path: /languages
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * @author      Hiram Huang <me@hiram.tw>
+ * @package     wp-content-restriction
+ * @license     http://www.gnu.org/licenses/gpl-3.0.txt
+ * @link        Github Repo: https://github.com/chown9835/wp-content-restriction
+ * @since       0.1
+ */
+
+// avoid direct access this file 
+if( ! defined( 'ABSPATH' ) ) {
+	die( 'SHAME ON YOU' );
+}
+
+/**
+ * Defines plugin named constant
+ */
+define( 'CR_PLUGIN_VERSION', 0.1 );
+define( 'CR_PLUGIN_PATH_FULL', __FILE__ );
+define( 'CR_PLUGIN_BASE_FULL', dirname(__FILE__) );
+define( 'CR_PLUGIN_BASE_RELATIVE', plugin_basename(__FILE__) );
+define( 'CR_PLUGIN_FILE_BASENAME', pathinfo( __FILE__, PATHINFO_FILENAME ) );
+define( 'CR_PLUGIN_TEXT_DOMAIN', 'wp-content-restriction' );
+
+/**
+ * Import plugin classed.
+ */
+
+
+/**
+ * Initial plugin.
+ */

ed46cb8a8a6a8d5fec3c889026a27559dad32953
Hiram Huang <chown9835@gmail.com>
2016-05-25 06:35:16 +0000

    Add file header to uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * WP Content Restriction
+ * 
+ * @author Hiram Huang <me@hiram.tw>
+ * @package wp-content-restriction
+ * @since 0.1
+ */
+
+// avoid direct access this file
+if( ! defined( 'ABSPATH' ) ) {
+	die( 'Do not direct access this file.' );
+}
+?>

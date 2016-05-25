fd776a0dda2fd09b75b01ce7680948a3ee508a64
Hiram Huang <chown9835@gmail.com>
2016-05-25 10:48:00 +0000

    Finish setting page templeate.

--- settings/template-wp-content-restriction-options.php
+++ settings/template-wp-content-restriction-options.php
@@ -1,3 +1,10 @@
 <div class="wrap">
-    <h2><?php _e( 'WP Content Restriction Settings', CR_PLUGIN_TEXT_DOMAIN ) ?></h2>
+    <h1><?php _e( 'WP Content Restriction Settings', CR_PLUGIN_TEXT_DOMAIN ) ?></h1>
+    <form action="options.php" method="post">
+        <?php
+            @settings_fields( self::CR_PLUGIN_SETTINGS_SLUG );
+            @do_settings_sections( self::CR_PLUGIN_SETTINGS_SLUG );
+            @submit_button();
+        ?>
+    </form>
 </div>

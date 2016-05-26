ca1a50f43db14449fb1115eb96a6eaed99e0ba6c
Hiram Huang <chown9835@gmail.com>
2016-05-26 08:11:04 +0000

    Restrict direct access template file.

--- settings/template-wp-content-restriction-options.php
+++ settings/template-wp-content-restriction-options.php
@@ -1,3 +1,16 @@
+<?php
+/**
+ * WP Content Restriction
+ * Global settings template.
+ * 
+ * @author Hiram Huang <me@hiram.tw>
+ * @since 0.1
+ */
+
+if( 0 >= CR_PLUGIN_VERSION ) {
+    return die( 'Do not direct access this file.' );
+}
+?>
 <div class="wrap">
     <h1><?php _e( 'WP Content Restriction Settings', CR_PLUGIN_TEXT_DOMAIN ) ?></h1>
     <form action="options.php" method="post">

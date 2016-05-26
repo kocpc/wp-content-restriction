375922f856d30b4b3bbdb24ccbc7eb6888aca1e3
Hiram Huang <chown9835@gmail.com>
2016-05-26 08:25:08 +0000

    Finish author options template.

--- settings/template-user-options.php
+++ settings/template-user-options.php
@@ -11,3 +11,24 @@ if( 0 >= CR_PLUGIN_VERSION ) {
     return die( 'Do not direct access this file.' );
 }
 ?>
+<h2><?php _e( 'Content Restriction', CR_PLUGIN_TEXT_DOMAIN ) ?></h2>
+<table class="form-table">
+    <tr>
+        <th>
+            <label><?php _e( 'Restirct Author Page', CR_PLUGIN_TEXT_DOMAIN ) ?></label>
+        </th>
+        <td>
+            <input type="checkbox" name="wpcr-restrict-author-page" value="1" <?php echo ( $current_settings['list'] ? 'checked' : null ) ?> />
+            <span> <?php _e( 'Select this option to restrict your author page.', CR_PLUGIN_TEXT_DOMAIN ) ?></span>
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label><?php _e( 'Restrict All Post', CR_PLUGIN_TEXT_DOMAIN ) ?></label>
+        </th>
+        <td>
+            <input type="checkbox" name="wpcr-restrict-all" value="1" <?php echo ( $current_settings['all'] ? 'checked' : null ) ?> />
+            <span> <?php _e( 'Select this option to restrict all your posts', CR_PLUGIN_TEXT_DOMAIN ) ?></span>
+        </td>
+    </tr>
+</table>

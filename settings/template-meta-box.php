95832b218a25daa4233cb6424f796ab67631ac67
Hiram Huang <chown9835@gmail.com>
2016-05-27 02:58:11 +0000

    Update meta box template, add restrict access and bug fixes.

--- settings/template-meta-box.php
+++ settings/template-meta-box.php
@@ -1,4 +1,17 @@
+<?php
+/**
+ * WP Content Restriction
+ * Render meta box on editor
+ * 
+ * @author Hiram Huang <me@hiram.tw>
+ * @since 0.1
+ */
+
+if( 0 >= CR_PLUGIN_VERSION ) {
+    return die( 'Do not direct access this file.' );
+}
+?>
 <p>
-    <input type="checkbox" name="wpcr-enable-restriction" value="1" <?php echo ( $current_status ? 'checked' : null ) ?> />
-    <label><?php _e( 'Restrict this post', CR_PLUGIN_TEXT_DOMAIN ) ?></label>
+    <input type="checkbox" name="wpcr-enable-restriction" id="wpcr-enable-restriction" value="1" <?php echo ( $current_status ? 'checked' : null ) ?> />
+    <label for="wpcr-enable-restriction"><?php _e( 'Restrict this post', CR_PLUGIN_TEXT_DOMAIN ) ?></label>
 </p>

627ebc67b446912b9a59beb516d5bef57ee251b6
Hiram Huang <chown9835@gmail.com>
2016-05-27 02:05:04 +0000

    Create and finish meta box template.

---
+++ settings/template-meta-box.php
@@ -0,0 +1,4 @@
+<p>
+    <input type="checkbox" name="wpcr-enable-restriction" value="1" <?php echo ( $current_status ? 'checked' : null ) ?> />
+    <label><?php _e( 'Restrict this post', CR_PLUGIN_TEXT_DOMAIN ) ?></label>
+</p>

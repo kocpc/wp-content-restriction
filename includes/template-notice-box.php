050e643b0b5c0d3c1caafa0946cdc0a465e2ed00
Hiram Huang <chown9835@gmail.com>
2016-05-30 04:38:57 +0000

    DOM identity use id replace class.

--- includes/template-notice-box.php
+++ includes/template-notice-box.php
@@ -11,16 +11,18 @@ if( 0 >= CR_PLUGIN_VERSION ) {
     return die( 'Do not direct access this file.' );
 }
 ?>
-<div class="wp-content-restriction-notice-box">
-    <div class="wp-content-restriction-notice-box-header">
-        <p class="wp-content-restriction-notice-box-title"><?php echo $value['title'] ?></p>
+<div id="wp-content-restriction-notice-box">
+    <div id="wp-content-restriction-notice-box-header">
+        <p id="wp-content-restriction-notice-box-title"><?php echo $value['title'] ?></p>
     </div>
-    <div class="wp-content-restriction-notice-box-body">
-        <p class="wp-content-restriction-notice-box-content"><?php echo $value['body'] ?></p>
+    <div id="wp-content-restriction-notice-box-body">
+        <p id="wp-content-restriction-notice-box-content"><?php echo $value['body'] ?></p>
     </div>
-    <div class="wp-content-restriction-notice-box-footer">
-        <button class="wp-content-restriction-notice-box-accept"><?php echo $value['accept'] ?></button>
-        <a class="wp-content-restriction-notice-box-decline" href="<?php echo $value['redirect'] ?>" rel="nofollow">
+    <div id="wp-content-restriction-notice-box-footer">
+        <button id="wp-content-restriction-notice-box-accept">
+            <?php echo $value['accept'] ?>
+        </button>
+        <a id="wp-content-restriction-notice-box-decline" href="<?php echo $value['redirect'] ?>" rel="nofollow">
             <?php echo $value['decline'] ?>
         </a>
     </div>

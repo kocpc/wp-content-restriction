d463612bb0680a896f8bdb2e1b91895eb8fa4632
Hiram Huang <chown9835@gmail.com>
2016-05-30 08:44:26 +0000

    Fix global settings field typo.

--- settings/class-wp-content-restriction-settings.php
+++ settings/class-wp-content-restriction-settings.php
@@ -190,7 +190,7 @@ class WP_Content_Restriction_Settings {
          */
         add_settings_section(
             'wpcr-shortcode',
-            __( 'Use Shortcode Enable Restriction' ),
+            __( 'Use Shortcode Enable Restriction', CR_PLUGIN_TEXT_DOMAIN ),
             null,
             self::CR_PLUGIN_SETTINGS_SLUG
         );
@@ -198,7 +198,7 @@ class WP_Content_Restriction_Settings {
         // shortcode list
         add_settings_field(
             'wpcr-shortcode-list',
-            __( 'Shortcode List ', CR_PLUGIN_TEXT_DOMAIN ),
+            __( 'Shortcode List', CR_PLUGIN_TEXT_DOMAIN ),
             array( 'WP_Content_Restriction_Settings_Field', 'element_shortcode_list' ),
             self::CR_PLUGIN_SETTINGS_SLUG,
             'wpcr-shortcode'

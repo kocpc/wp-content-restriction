bffd5206af8576bee003887599707904a2db6807
Hiram Huang <chown9835@gmail.com>
2016-05-30 04:20:13 +0000

    Create notice box template and initial.

---
+++ includes/template-notice-box.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * WP Content Restriction
+ * Render notice box
+ * 
+ * @author Hiram Huang <me@hiram.tw>
+ * @since 0.1
+ */
+
+if( 0 >= CR_PLUGIN_VERSION ) {
+    return die( 'Do not direct access this file.' );
+}
+?>

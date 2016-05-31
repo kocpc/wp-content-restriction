4a8ada8bbcc1df48791fada98f9fcb2890b2a197
Hiram Huang <chown9835@gmail.com>
2016-05-31 02:26:16 +0000

    Restrict directly access file, return 403 error.

--- includes/template-notice-box.php
+++ includes/template-notice-box.php
@@ -7,8 +7,13 @@
  * @since 0.1
  */
 
-if( 0 >= CR_PLUGIN_VERSION ) {
-    return die( 'Do not direct access this file.' );
+/**
+ * If file is opened directly, return 403 error
+ */
+if( ! function_exists ('add_action') ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
 }
 ?>
 <div class="wp-content-restriction-notice-box">
